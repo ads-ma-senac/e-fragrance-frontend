98375654f5b96baeff1ecac22aea57edddc4bd84
LUCAS GUEDES MORATO SILVA <lucas.gmsilva9@senacsp.edu.br>
2025-04-30 09:19:31 -0300

    adicionando routerlink dinamico com for na sidenav

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ListagemProdutosComponent } from './features/listagem-produtos/listagem-produtos.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/produtos', pathMatch: 'full' },
-  { path: 'produtos', component: ListagemProdutosComponent }
+  {
+    path: "produtos", component: ListagemProdutosComponent
+  }
 ];
--- frontend/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -2,15 +2,41 @@ import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { ListagemProdutosComponent } from '../../features/listagem-produtos/listagem-produtos.component';
+
+type link = { path: string, label: string};
 
 @Component({
   selector: 'app-sidebar',
   standalone:true,
   imports: [MatSidenavModule,
-    MatListModule],
+            MatListModule, 
+            RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
 
+export class SidebarComponent {
+  links: link[] = [
+    {
+      path: "/home",
+      label: "home"
+    },
+    {
+      path: "/dashboard",
+      label: "dashboard"
+    },
+    {
+      path: "/vendas",
+      label: "vendas"
+    },
+    {
+      path: "/produtos",
+      label: "produtos"
+    },
+    {
+      path: "/usuarios",
+      label: "usuarios"
+    }
+  ]
 }
