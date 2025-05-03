913811a616963137b91805fd5ba4411be6f508b4
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-05-03 03:12:06 +0000

    Atualização do componente de login

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,16 +1,47 @@
-import {Component,} from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
-
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, NgOptimizedImage],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    NgOptimizedImage
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  coverUrl = '/cover.png';
+  coverUrl = 'cover.png';
+  fragranceUrl = 'fragrance.png';
+  loginForm: FormGroup;
+  loginInvalid = false;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+
+    if (email === 'admin.adm@gmail.com' && password === 'adm123') {
+      this.loginInvalid = false;
+      this.router.navigate(['/home']);
+    } else {
+      this.loginInvalid = true;
+    }
+  }
 }
