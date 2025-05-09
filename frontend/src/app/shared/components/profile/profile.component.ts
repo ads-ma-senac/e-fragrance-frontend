cb9900ebb45cafa628c47adc807072d4adf1bae8
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-05-09 01:43:44 -0300

    adicionado componente de perfil com estilização e integração no sidebar

---
+++ frontend/src/app/shared/components/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile',
+  imports: [CommonModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class ProfileComponent {
+  private authService = inject(AuthService);
+  usuario = this.authService.getUsuario();
+}
--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,42 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProfileComponent } from '../profile/profile.component';
 import { RouterModule } from '@angular/router';
-import { ListarComponent } from '../../../features/admin/produtos/listagem-produtos/listar.component';
 
-type link = { path: string, label: string};
+type link = { path: string; label: string };
 
 @Component({
   selector: 'app-sidebar',
-  standalone:true,
-  imports: [MatSidenavModule,
-            MatListModule,
-            RouterModule],
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    CommonModule,
+    ProfileComponent,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
-
 export class SidebarComponent {
   links: link[] = [
     {
-      path: "/home",
-      label: "home"
+      path: '/home',
+      label: 'home',
     },
     {
-      path: "/dashboard",
-      label: "dashboard"
+      path: '/dashboard',
+      label: 'dashboard',
     },
     {
-      path: "/vendas",
-      label: "vendas"
+      path: '/vendas',
+      label: 'vendas',
     },
     {
-      path: "/admin/produtos",
-      label: "produtos"
+      path: '/admin/produtos',
+      label: 'produtos',
     },
     {
-      path: "/usuarios",
-      label: "usuarios"
-    }
-  ]
+      path: '/usuarios',
+      label: 'usuarios',
+    },
+  ];
 }
