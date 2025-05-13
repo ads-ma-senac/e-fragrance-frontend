62de5adb93f84024b4a897c87aa7a26da04e2ca3
Paula Zillig <166560940+pzig@users.noreply.github.com>
2025-05-13 13:37:19 +0000

    estilizando header e hero

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatToolbarModule,
-    MatIconModule,],
+    MatIconModule,
+  NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  fragranceUrl = 'assets/icons/fragrance.svg';
 }
--- frontend/src/app/shared/components/hero/hero.component.ts
+++ frontend/src/app/shared/components/hero/hero.component.ts
@@ -1,12 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  heroUrl = 'hero.png';
 }
