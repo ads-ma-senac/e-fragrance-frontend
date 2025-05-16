7039ef798af9684b4decc9a7767ec26fb08b796c
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-05-15 23:48:12 -0300

    adicionando ícones de navegação e estilizando ações do cabeçalho

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule,
-    MatIconModule,
-  NgOptimizedImage],
+  imports: [MatToolbarModule, MatIconModule, NgOptimizedImage],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   fragranceUrl = 'assets/icons/fragrance.svg';
+  searchIcon = 'assets/icons/search.svg';
+  shoppingBagIcon = 'assets/icons/shopping-bag.svg';
+  userIcon = 'assets/icons/user.svg';
+  starIcon = 'assets/icons/star.svg';
 }
